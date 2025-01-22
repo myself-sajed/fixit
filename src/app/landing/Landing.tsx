5aa8e505f5e3d651629865c9c43764b1fca42dcd
Shaikh Sajed <85938068+myself-sajed@users.noreply.github.com>
2025-01-22 16:02:56 +0530

    Create Landing.tsx

---
+++ src/app/landing/Landing.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const Landing = () => {
+    return (
+        <div>
+            this is landing page
+            <Button>Get Started</Button>
+        </div>
+    )
+}
+
+export default Landing
