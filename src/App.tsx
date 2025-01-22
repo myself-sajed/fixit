f24bd6b0ddd6235531a711ab2001e880722b97f4
sajed-evsp <sajed@almochat.com>
2025-01-22 15:25:46 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button"
+
 const App = () => {
   return (
     <div>
       Welcome to FixIt
+      <div>
+        <Button>Get Started</Button>
+      </div>
     </div>
   )
 }
